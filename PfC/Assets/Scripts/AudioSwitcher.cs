e6a94786eea524744680a65ac17c26df91b9993f
Olviia Stroivans <olviia.stroivans@gmail.com>
2025-03-25 14:42:14 +0000

    Launched PfC, added gitignore, added AudioSwitcher script

---
+++ PfC/Assets/Scripts/AudioSwitcher.cs
@@ -0,0 +1,101 @@
+using Klak.Ndi;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class AudioSwitcher : MonoBehaviour
+{
+    [SerializeField] private NdiReceiver ndiReceiver;
+
+    // Enum to represent the different audio modes
+    public enum AudioMode
+    {
+        Automatic = 0,
+        VirtualSpeakers = 1,
+        None = 2
+    }
+
+    private AudioMode currentMode = AudioMode.Automatic;
+
+    private void Start()
+    {
+        if (ndiReceiver == null)
+        {
+            Debug.LogError("NdiReceiver reference not set in AudioModeToggle component");
+            return;
+        }
+
+    }
+
+    // Call this method from your UI button
+    public void ToggleAudioMode()
+    {
+        // Cycle through modes: Automatic -> Virtual Speakers -> None -> Automatic
+        currentMode = (AudioMode)(((int)currentMode + 1) % 3);
+
+        ApplyAudioMode();
+    }
+
+    // Set a specific mode directly
+    public void SetAudioMode(AudioMode mode)
+    {
+        currentMode = mode;
+        ApplyAudioMode();
+    }
+
+    private void ApplyAudioMode()
+    {
+        // Access the serialized fields directly
+        // Note: In builds, you won't have the editor GUI functionality,
+        // so we need to modify the component properties directly
+
+        switch (currentMode)
+        {
+            case AudioMode.Automatic:
+                // Automatic mode
+                SetReceiveAudioField(true);
+                SetCreateVirtualSpeakersField(false);
+                break;
+
+            case AudioMode.VirtualSpeakers:
+                // Always create Virtual Speakers
+                SetReceiveAudioField(true);
+                SetCreateVirtualSpeakersField(true);
+                break;
+
+            case AudioMode.None:
+                // No audio
+                SetReceiveAudioField(false);
+                break;
+        }
+
+        // Trigger any necessary updates in the NdiReceiver
+        //ndiReceiver.Restart();
+
+    }
+
+    // Helper method to set the _createVirtualSpeakers field via reflection
+    // (since it's not exposed as a public property)
+    private void SetCreateVirtualSpeakersField(bool value)
+    {
+        var field = typeof(NdiReceiver).GetField("_createVirtualSpeakers",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (field != null)
+            field.SetValue(ndiReceiver, value);
+    }
+
+    private void SetReceiveAudioField(bool value)
+    {
+        var field = typeof(NdiReceiver).GetField("_receiveAudio",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (field != null)
+            field.SetValue(ndiReceiver, value);
+        else
+            Debug.LogError("Could not find _receiveAudio field in NdiReceiver");
+    }
+}
