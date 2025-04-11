6153ecbbebc5ec9e5e31911ca9da09fc8269d201
denizmev <deniz.mevlevioglu@tus.ie>
2025-04-11 15:01:57 +0100

    added visual layers for studio/director. Ray interactors still need to be changed

--- PfC/Assets/Scripts/NetworkSceneManager.cs
+++ PfC/Assets/Scripts/NetworkSceneManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.UI;
+using UnityEngine.XR.Interaction.Toolkit.Utilities;
 
 public class NetworkSceneManager : NetworkBehaviour
 {
     [SerializeField] private string hostScene;
     [SerializeField] private string clientScene;
     [SerializeField] private GameObject networkObject;
+    //[SerializeField] private GameObject leftRayInteractor;
+    //[SerializeField] private GameObject rightRayInteractor;
     private NetworkManager networkManager;
 
    
@@ -15,29 +20,30 @@ public class NetworkSceneManager : NetworkBehaviour
     {
         networkManager = networkObject.GetComponent<NetworkManager>();
 
+        var status = NetworkManager.SceneManager.LoadScene(hostScene,
+                                                   UnityEngine.SceneManagement.LoadSceneMode.Single);
+
+        if (status != SceneEventProgressStatus.Started)
+        {
+            Debug.LogWarning($"Failed to load {hostScene} " +
+                  $"with a {nameof(SceneEventProgressStatus)}: {status}");
+        }
+
+        //var raycastMaskLeft = leftRayInteractor.GetComponent<XRRayInteractor>();
+        //var raycastMaskRight = rightRayInteractor.GetComponent<XRRayInteractor>();
+
         if (networkManager.IsServer)
         {
             Debug.Log("Loading host Scene");
-            var status = NetworkManager.SceneManager.LoadScene(hostScene, 
-                                                               UnityEngine.SceneManagement.LoadSceneMode.Single);
-
-            if (status != SceneEventProgressStatus.Started)
-            {
-                Debug.LogWarning($"Failed to load {hostScene} " +
-                      $"with a {nameof(SceneEventProgressStatus)}: {status}");
-            }
+            var layerMask = LayerMask.NameToLayer("Director");
+            Camera.main.cullingMask |= (1 << layerMask);
+            //raycastMaskLeft.raycastMask |= (1 << layerMask);
         } 
         else
         {
             Debug.Log("Loading client Scene");
-            var status = NetworkManager.SceneManager.LoadScene(clientScene,
-                                                   UnityEngine.SceneManagement.LoadSceneMode.Single);
-
-            if (status != SceneEventProgressStatus.Started)
-            {
-                Debug.LogWarning($"Failed to load {clientScene} " +
-                      $"with a {nameof(SceneEventProgressStatus)}: {status}");
-            }
+            var layerMask = LayerMask.NameToLayer("Studio");
+            Camera.main.cullingMask |= (1 << layerMask);
         }
     }
 }
