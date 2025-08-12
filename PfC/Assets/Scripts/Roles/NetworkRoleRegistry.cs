9b019c5f011a4ab95d9d5c5c47a3b8c02f2b010e
Olviia Stroivans <olviia.stroivans@gmail.com>
2025-08-12 17:26:11 +0100

    proper registering of ids for vivox

--- PfC/Assets/Scripts/Roles/NetworkRoleRegistry.cs
+++ PfC/Assets/Scripts/Roles/NetworkRoleRegistry.cs
@@ -4,96 +4,74 @@ using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
+public struct VivoxUserRole : INetworkSerializable, System.IEquatable<VivoxUserRole>
+{
+    public FixedString64Bytes  playerId;
+    public Role role;
+    
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref playerId);
+        serializer.SerializeValue(ref role);
+    }
+    
+    public bool Equals(VivoxUserRole other) => playerId.Equals(other.playerId);
+}
 public class NetworkRoleRegistry : NetworkBehaviour
 {
-    // Network-synced dictionary of role mappings
-    private NetworkList<RoleMapping> roleMappings;
-
-    [System.Serializable]
-    public struct RoleMapping : INetworkSerializable
+    private NetworkList<VivoxUserRole> userRoles;
+    
+    public static NetworkRoleRegistry Instance { get; private set; }
+    void Awake()
     {
-        public Role role;
-        public ulong netcodeClientId;
-        public FixedString128Bytes vivoxParticipantId;
-        public FixedString64Bytes displayName;
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        // Singleton setup
+        if (Instance != null && Instance != this)
         {
-            serializer.SerializeValue(ref role);
-            serializer.SerializeValue(ref netcodeClientId);
-            serializer.SerializeValue(ref vivoxParticipantId);
-            serializer.SerializeValue(ref displayName);
+            Destroy(gameObject);
+            return;
         }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        // Initialize the NetworkList
+        userRoles = new NetworkList<VivoxUserRole>();
     }
     
-    public override void OnNetworkSpawn()
-    {
-        if (IsServer)
-        {
-            roleMappings = new NetworkList<RoleMapping>();
-        }
-        
-        // Subscribe to changes so all clients can react
-        roleMappings.OnListChanged += OnRoleMappingsChanged;
-    }
-    // Called when a player joins Vivox channel and gets their participant ID
     [ServerRpc(RequireOwnership = false)]
-    public void RegisterPlayerServerRpc(Role role, ulong clientId, string vivoxParticipantId, string displayName)
+    public void RegisterRoleServerRpc(Role role, string playerId)
     {
-        Debug.Log($"[NetworkRoleRegistry] Registering: {role} -> NetcodeID:{clientId}, VivoxID:{vivoxParticipantId}");
+        // First, remove any existing role for this player
+        RemovePlayerFromList(playerId);
         
-        // Remove any existing entry for this client
-        for (int i = roleMappings.Count - 1; i >= 0; i--)
+        // Then add the new role
+        var newUserRole = new VivoxUserRole 
+        { 
+            playerId = playerId, 
+            role = role 
+        };
+        
+        userRoles.Add(newUserRole); // Add to NetworkList
+        Debug.Log($"Registered {playerId} as {role}");
+       
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void UnregisterRoleServerRpc(string playerId)
+    {
+        RemovePlayerFromList(playerId);
+    }
+    
+    // Helper method to remove player from list
+    private void RemovePlayerFromList(string playerId)
+    {
+        // Loop backwards to safely remove while iterating
+        for (int i = userRoles.Count - 1; i >= 0; i--)
         {
-            if (roleMappings[i].netcodeClientId == clientId)
+            if (userRoles[i].playerId == playerId)
             {
-                roleMappings.RemoveAt(i);
+                userRoles.RemoveAt(i);
+                break; // Only one entry per player should exist
             }
         }
-        
-        // Add new mapping
-        roleMappings.Add(new RoleMapping
-        {
-            role = role,
-            netcodeClientId = clientId,
-            vivoxParticipantId = vivoxParticipantId,
-            displayName = displayName
-        });
     }
-    
-    // Get Vivox participant ID for a specific role
-    public string GetVivoxIdForRole(Role targetRole)
-    {
-        foreach (var mapping in roleMappings)
-        {
-            if (mapping.role == targetRole)
-            {
-                return mapping.vivoxParticipantId.ToString();
-            }
-        }
-        return null;
-    }
-    
-    // Get all current mappings (useful for debugging)
-    public Dictionary<Role, string> GetAllRoleMappings()
-    {
-        var result = new Dictionary<Role, string>();
-        foreach (var mapping in roleMappings)
-        {
-            result[mapping.role] = mapping.vivoxParticipantId.ToString();
-        }
-        return result;
-    }
-    
-    private void OnRoleMappingsChanged(NetworkListEvent<RoleMapping> changeEvent)
-    {
-        Debug.Log($"[NetworkRoleRegistry] Role mappings updated. Event: {changeEvent.Type}");
-        
-        // Notify CommunicationManager about role changes
-        if (changeEvent.Type == NetworkListEvent<RoleMapping>.EventType.Add)
-        {
-            var newMapping = changeEvent.Value;
-            CommunicationManager.Instance?.OnPlayerRoleRegistered(newMapping.role, newMapping.vivoxParticipantId.ToString());
-        }
-    }
+
 }
